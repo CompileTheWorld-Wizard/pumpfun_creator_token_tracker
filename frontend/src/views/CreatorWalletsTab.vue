b9412fd55c572dd9d284c919a08cc412e39756a5
DavitK000000 <davitkhimshiashvili96@gmail.com>
2025-12-15 06:59:11 -0800

    Added creator wallet analysis page

--- frontend/src/views/CreatorWalletsTab.vue
+++ frontend/src/views/CreatorWalletsTab.vue
@@ -22,13 +22,13 @@
             @click="toggleViewMode"
             :class="[
               'relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2',
-              viewMode === 'data' ? 'bg-purple-600' : 'bg-gray-600'
+              viewMode === 'score' ? 'bg-purple-600' : 'bg-gray-600'
             ]"
           >
             <span
               :class="[
                 'inline-block h-4 w-4 transform rounded-full bg-white transition-transform',
-                viewMode === 'data' ? 'translate-x-6' : 'translate-x-1'
+                viewMode === 'score' ? 'translate-x-6' : 'translate-x-1'
               ]"
             ></span>
           </button>
