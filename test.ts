28963c7993c0ae0df213687a9b9edbc5cd23e02e
DavitK000000 <davitkhimshiashvili96@gmail.com>
2026-01-08 03:16:51 -0800

    Added a test script

---
+++ test.ts
@@ -0,0 +1,25 @@
+import { Parser, TransactionStreamer } from "@shyft-to/ladybug-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+const parser = new Parser();
+// Initializing parser with Pump.fun IDL
+
+const streamer = new TransactionStreamer(process.env.GRPC_URL!, process.env.X_TOKEN);
+streamer.addParser(parser);
+// setting up streamer to parse using the Pump.fun parser
+
+streamer.addAddresses(["6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P"]);
+// adds the address to stream data from
+
+async function processData(processed) {
+    //handle the incoming transaction
+    console.log("Received Data: ");
+    console.log(processed);
+  }
+
+streamer.onData(processData);
+// set the callback function that receives the data
+
+streamer.start();
+//starts streaming transactions
